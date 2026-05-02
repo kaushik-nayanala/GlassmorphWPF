8c92de73d08266616c4a72fc1c654c9523210cc6
Kaushik <nayanalakaushik@gmail.com>
2026-05-02 16:11:24 +0530

    Added dropshadow effect and also made border react with bacground with some refraction.

--- Maindev/Maindev/App.xaml.cs
+++ Maindev/Maindev/App.xaml.cs
@@ -47,4 +47,37 @@ namespace Maindev
         }
 
     }
+
+    public class VisualBrushScaleConverterBorder : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 6) return Transform.Identity;
+            double BEWt = (double)values[0];
+            double BEHt = (double)values[1];
+
+            double BIWt = (double)values[2];
+            double BIHt = (double)values[3];
+
+            double posx = (double)values[4];
+            double posy = (double)values[5];
+
+            double scalex = BIWt / BEWt;
+            double scaley = BIHt / BEHt;
+
+            double centerx = -(posx-2) / BEWt;
+            double centery = -(posy-2) / BEHt;
+
+
+            var tg = new TransformGroup();
+            tg.Children.Add(new ScaleTransform(scalex, scaley, 0, 0));
+            tg.Children.Add(new TranslateTransform(centerx, centery));
+            return tg;
+        }
+        public object[] ConvertBack(object values, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
 }
